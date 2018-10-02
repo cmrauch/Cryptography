9e04c5ccf9e2d0ead45a4969fde72bfb7224da24
Christopher Rauch <38539086+cmrauch@users.noreply.github.com>
2018-10-02 14:20:27 -0700

    Update FairPlay.py

--- FairPlay.py
+++ FairPlay.py
@@ -8,31 +8,53 @@ Exercise # 2:
     possible plaintexts in rough order of likelihood. It would be good if your user
 i   nterface allowed the user to specify “give me the top 5 possible plaintexts.
 '''
+def init_matrix():
+  n = 5
+  m = 5
+  a = ['a'] * n
+  for i in range(n):
+    a[i] = ['a'] * m
+  return a
+
 def createMatrix(key):
     
-    a = 'a'
+    a = 97
     key_count = 0
     key_len = len(key)
+    matrix = init_matrix()
     #this list keeps track of the letters have already been used in the key
     alphabet = [False]*26 
     
     #iterate through the matrix key
     for i in range(0,5):
         for j in range(0,5):
-            #check to see if letter has already been inserted into the matrix
-            if alphabet[ord(key[key_count]) - 97] == False or alphabet[a - 97] == False: #check to see if the next letter is already in the matrix
-                #fill matrix with key
-                if (key_count < key_len): # potential bug --> see if index is correct
-                    matrix[i][j] = key[key_count]
-                    key_count += 1
-                    alphabet[ord(key[key_count]) - 97] = True #mark the letter as used
-                #fill matrix with alphabet
-                else:
-                    matrix[i][j] = a
-                    a += chr(ord(a) + 1)
-                    alphabet[a - 97] == True #mark the letter as used
-         
+          
+          #check for duplicates
+          
+                   
+          if key_count < key_len:
+          #add key
+            if not alphabet[ord(key[key_count]) - 97]:
+              matrix[i][j] = key[key_count]
+              alphabet[ord(key[key_count]) - 97] = True
+            key_count = key_count + 1       
+          #add alphabet
+          elif a < 123:
+            if not alphabet[a - 97]:
+              matrix[i][j] = chr(a)
+              print('or here')
+              alphabet[(a - 97)] = True
+            a = a + 1#increment a
+          
+          
     return matrix
+  
+plaintext = list(input("Enter the Plaintext: "))
+key = list(input("Enter the keyword: "))
+
+matrix = createMatrix(key)
+
+print(matrix)
 #===================================================================================================================    
 
 plaintext = list(input("Enter the Plaintext: "))
